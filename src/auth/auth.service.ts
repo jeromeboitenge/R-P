022f87abf1a5e95084599012cb427e7af8a94f23
boitenge <jeromeboitenge@gmail.com>
2026-01-19 09:36:18 +0200

    adding auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-signup.dto';
+import { CreateSIgnupDto } from './dto/create-signup.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 
 @Injectable()
@@ -8,19 +8,4 @@ export class AuthService {
     return 'This action adds a new auth';
   }
 
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
 }
