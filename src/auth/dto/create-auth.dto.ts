e7fffc254695193e35c64c9ce56941429d85911f
boitenge <jeromeboitenge@gmail.com>
2026-01-19 08:39:18 +0200

    creating signup dto

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,9 @@
-export class CreateAuthDto {}
+import { userRoles } from "src/enums/enums"
+
+export class CreateSIgnupDto {
+    name: string
+    email: string
+    password: string
+    role?: userRoles
+    department: string
+}
---
+++ src/enums/enums.ts
@@ -0,0 +1,7 @@
+export enum userRoles {
+    Manager = 'manager',
+    Admin = 'admin',
+    User = 'user',
+    Finance = 'finance'
+
+}
